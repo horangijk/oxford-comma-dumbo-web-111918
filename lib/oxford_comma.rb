404cf4c059ad1e542fdd3db88b9bea48bb4ddcb7
Jerold Kim <iamjooyoung@gmail.com>
2018-11-11 02:23:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,4 @@
 def oxford_comma(array)
   if array.length == 1 
-    return array[0]
-  elsif array.length == 2 
-    return array[0] + " and " + array [1]
-  else 
-    return array[0] + ", " + array[1]
+    array.join
 end
